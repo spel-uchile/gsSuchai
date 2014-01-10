40554b932d20872bace70f35d87ba40e5bbb8608
root <root@estacion-proto>
2014-01-10 10:42:45 -0300

    Actualizacion a la version funcional con kiss.c

--- gsAntenna/kiss.c
+++ gsAntenna/kiss.c
@@ -29,6 +29,7 @@
 
 CSP_DEFINE_TASK(task_server) {
 
+	//libcsp init
     int running = 1;
     csp_socket_t *socket = csp_socket(CSP_SO_NONE);
     csp_conn_t *conn;
@@ -51,28 +52,60 @@ CSP_DEFINE_TASK(task_server) {
 	int i;
 	int obufflen;
 
+	//ZMQ init
+    //  Prepare our context and publisher
+    void *context = zmq_ctx_new ();
+    void *publisher = zmq_socket (context, ZMQ_PUB);
+    int rc = zmq_bind (publisher, "tcp://*:5556");
+    assert (rc == 0);
+    rc = zmq_bind (publisher, "ipc://weather.ipc");
+    assert (rc == 0);
 
+    //  Initialize random number generator
+    srandom ((unsigned) time (NULL));
+    char update [200];
+	char bf[200];
+    int zipcode, temperature, relhumidity;
+
+
+	// listen incomming packets
     while(running) {
         if( (conn = csp_accept(socket, 10000)) == NULL ) {
             continue;
         }
-	    printf("llego un paquete, ping, etc.. \r\n");
         while( (packet = csp_read(conn, 100)) != NULL ) {
             switch( csp_conn_dport(conn) ) {
                 case PORT:
-                   //if( packet->data[0] == 'q' )
-                        //running = 0;
-					if(PACKET_ASCII==1){
-					    printf("%s", packet->data );
+   					printf("task_server: Received CSP packet in address %d (len = %d)\n", D_ADDRESS, packet->length);
+
+					obufflen = packet->length;
+					if(PACKET_ASCII==1){				
+						for(i=0;i<obufflen;i++){
+						    printf("%c", packet->data[i] );
+						}
+				    	printf("\n");
 					}
 					else{
-						obufflen = packet->length;
 						for(i=0;i<obufflen;i++){
 						    printf("0x%X", packet->data[i] );
 						}
 				    	printf("\n");
 					}
+				
+					//send to gsRemote
+   					printf("task_server: Resending as ZMQ packet\n");
 
+					for(i=0;i<(packet->length);i++){
+						bf[i] = packet->data[i];
+					}
+					bf[i]='\0';
+
+ 			       	// Send message to all "TM:" subscribers
+			        sprintf (update, "TM: %s", bf);
+					//printf(update); printf("\n");
+			        s_send (publisher, update);
+
+					// libcsp tasks
                     csp_buffer_free(packet);
                     csp_send(conn, response, 1000);
                     break;
@@ -87,11 +120,16 @@ CSP_DEFINE_TASK(task_server) {
 
     csp_buffer_free(response);
 
+    zmq_close (publisher);
+    zmq_ctx_destroy (context);
+
+
     return CSP_TASK_RETURN;
 }
 
 CSP_DEFINE_TASK(task_client) {
 
+	//libcsp
     char outbuf = 'q';
     char outbuf2 = 'a';
     char inbuf[3] = {0};
@@ -110,6 +148,36 @@ CSP_DEFINE_TASK(task_client) {
 	}
     printf("sizeof(data) = %d\n", obufflen );
 
+	//ZMQ
+    //  Socket to receive messages on
+    void *context = zmq_ctx_new ();
+    void *receiver = zmq_socket (context, ZMQ_PULL);
+    zmq_bind (receiver, "tcp://*:5557");
+
+    //  Process tasks forever
+	int st;
+    while (1) {
+		//int zmq_send (void *socket, void *buf, size_t len, int flags);
+		//int zmq_recv (void *socket, void *buf, size_t len, int flags);
+
+		obufflen = 255;
+		st = zmq_recv (receiver, obuff, obufflen, 0);
+		if(st < obufflen){
+			obufflen = st;	//number of receiver bytes (truncated if st>obufflen)
+		}
+		printf("task_client: Received ZMQ packet (len = %d)\n", obufflen);
+		printf("task_client: Resending as CSP packet to address %d\n", D_ADDRESS);
+
+        //char *string = s_recv (receiver);
+        //printf ("%s", string);     //  Show progress
+        //fflush (stdout);
+        //free (string);
+
+    	csp_transaction(0, D_ADDRESS, PORT, 1000, obuff, obufflen, ibuff, -1);
+	    printf("task_client: Response from CSP %d address: %s\n", D_ADDRESS, ibuff);
+    }
+    zmq_close (receiver);
+    zmq_ctx_destroy (context);
 
 	/*
     for(int i = 50; i <= 200; i+= 50) {
@@ -131,135 +199,13 @@ CSP_DEFINE_TASK(task_client) {
     //csp_transaction(0, D_ADDRESS, PORT, 1000, &outbuf, 1, inbuf, 2);
     //printf("Quit response from server: %s\n", inbuf);
 
-    printf("ready to receive packet->buffer\n");
-	while(1){
-		char *msi = mgetline();
-		int lmsi = strlen(msi)+1;
-	    printf("msi = %s\n", msi);
-	    printf("lmsi =  %d\n", lmsi);
-		/*
-		FILE *fp = fopen("log.txt", "ab+");
-		if(fp == NULL) //if file does not exist, create it
-		{
-			//do something about it
-			return CSP_TASK_RETURN;
-		}
-		fprintf(fp, "Some text: %s\n", msi);
-		fclose(fp);
-		*/
-    	csp_transaction(0, D_ADDRESS, PORT, 1000, msi, lmsi, ibuff, -1);
-	    printf("Quit response from server: %s\n", ibuff);
-		free(msi);
-	}
-
-	
     return CSP_TASK_RETURN;
-}
-
-CSP_DEFINE_TASK(task_tx) {
-	printf("task_tx\n");
-
- 	printf ("Connecting to hello world server…\n");
-    void *context = zmq_ctx_new();
-    void *requester = zmq_socket(context, ZMQ_REQ);
-    zmq_connect(requester, "tcp://localhost:5555");
-
-    int request_nbr;
-    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-        char buffer [10];
-        printf("Sending Hello %d…\n", request_nbr);
-        zmq_send(requester, "Hello", 5, 0);
-        zmq_recv(requester, buffer, 10, 0);
-        printf("Received World %d\n", request_nbr);
-    }
-    zmq_close(requester);
-    zmq_ctx_destroy(context);
-  
-    return 0;
-}
-CSP_DEFINE_TASK(task_rx) {
-	printf("task_rx\n");
-
- 	//  Socket to talk to clients
-    void *context = zmq_ctx_new();
-    void *responder = zmq_socket(context, ZMQ_REP);
-    int rc = zmq_bind(responder, "tcp://*:5555");
-    assert(rc == 0);
-
-    while (1) {
-        char buffer [10];
-        zmq_recv(responder, buffer, 10, 0);
-        printf("Received Hello\n");
-        sleep(1);          //  Do some 'work'
-        zmq_send(responder, "World", 5, 0);
-    }
-
-    return 0;
-}
-CSP_DEFINE_TASK(task_pub) {
-    //  Prepare our context and publisher
-    void *context = zmq_ctx_new ();
-    void *publisher = zmq_socket (context, ZMQ_PUB);
-    int rc = zmq_bind (publisher, "tcp://*:5556");
-    assert (rc == 0);
-    rc = zmq_bind (publisher, "ipc://weather.ipc");
-    assert (rc == 0);
-
-    //  Initialize random number generator
-    srandom ((unsigned) time (NULL));
-    while (1) {
-        //  Get values that will fool the boss
-        int zipcode, temperature, relhumidity;
-        zipcode     = randof (100000);
-        temperature = randof (215) - 80;
-        relhumidity = randof (50) + 10;
-
-        //  Send message to all subscribers
-        char update [20];
-        sprintf (update, "%05d %d %d", zipcode, temperature, relhumidity);
-        s_send (publisher, update);
-    }
-    zmq_close (publisher);
-    zmq_ctx_destroy (context);
-    return 0;
-}
-CSP_DEFINE_TASK(task_sub) {
-    //  Socket to talk to server
-    printf ("Collecting updates from weather server…\n");
-    void *context = zmq_ctx_new ();
-    void *subscriber = zmq_socket (context, ZMQ_SUB);
-    int rc = zmq_connect (subscriber, "tcp://localhost:5556");
-    assert (rc == 0);
-
-    //  Subscribe to zipcode, default is NYC, 10001
-    char *filter = "10001 ";
-    rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filter, strlen (filter));
-    assert (rc == 0);
-
-    //  Process 100 updates
-    int update_nbr;
-    long total_temp = 0;
-    for (update_nbr = 0; update_nbr < 100; update_nbr++) {
-        char *string = s_recv (subscriber);
-
-        int zipcode, temperature, relhumidity;
-        sscanf (string, "%d %d %d",
-            &zipcode, &temperature, &relhumidity);
-        total_temp += temperature;
-        free (string);
-    }
-    printf ("Average temperature for zipcode '%s' was %dF\n",
-        filter, (int) (total_temp / update_nbr));
-
-    zmq_close (subscriber);
-    zmq_ctx_destroy (context);
-    return 0;
 }
 
 int main(int argc, char **argv) {
   
-	csp_debug_toggle_level(CSP_PACKET);
-    csp_debug_toggle_level(CSP_INFO);
+	//csp_debug_toggle_level(CSP_PACKET);
+    //csp_debug_toggle_level(CSP_INFO);
 
     csp_buffer_init(10, 300);
     csp_init(S_ADDRESS);
@@ -299,19 +245,6 @@ int main(int argc, char **argv) {
 
     csp_thread_handle_t handle_client;
     csp_thread_create(task_client, (signed char *) "CLIENT", 1000, NULL, 0, &handle_client);
-
-    csp_thread_handle_t handle_tx;
-    csp_thread_create(task_tx, (signed char *) "TX", 1000, NULL, 0, &handle_tx);
-	csp_sleep_ms(2000);
-    csp_thread_handle_t handle_rx;
-    csp_thread_create(task_rx, (signed char *) "RX", 1000, NULL, 0, &handle_rx);
-
-	csp_sleep_ms(11000);
-    csp_thread_handle_t handle_pub;
-    csp_thread_create(task_pub, (signed char *) "PUB", 1000, NULL, 0, &handle_pub);
-	csp_sleep_ms(2000);
-    csp_thread_handle_t handle_sub;
-    csp_thread_create(task_sub, (signed char *) "SUB", 1000, NULL, 0, &handle_sub);
 
     /* Wait for program to terminate (ctrl + c) */
     while(1) {
