102f82f31db5cf17847c7086d83e1f4aab18f29c
carlgonz <contacto.carlosgonzalez@gmail.com>
2017-08-24 13:39:40 +0000

    Updating version

--- gsRemote/setup.py
+++ gsRemote/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='groundstation',
-    version='0.4.4',
+    version='0.4.5',
     url='https://github.com/carlgonz/SerialCommander',
     license='GPL v3',
     author='Carlos Gonzalez',
